352fdebab92983281aa44cbe363b4e56da77cb62
tony feng <thefunkyphresh@gmail.com>
2024-03-30 13:13:13 -0400

    add render function to solid

--- src/solid/index.js
+++ src/solid/index.js
@@ -1,17 +1,24 @@
 class Solid {
 	constructor ({
-		components = [],
+		children = [],
+    materialize = () => {},
 		name = "solid",
 		position = [0, 0, 0],
 		pointsOfInterest = {},
 		transformations = [],
 	}) {
-		this.components = components;
+		this.children = children;
+    this.materialize = materialize;
 		this.name = name;
 		this.position = position;
 		this.pointsOfInterest = pointsOfInterest;
 		this.transformations = transformations;
-	};
 
-	
+    this.render = async () => {
+      await this.materialize();
+      await Promise.all(children.map(async (solid) => { await solid.render() }));
+    };
+	};
 };
+
+module.exports = Solid;
