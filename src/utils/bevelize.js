f2a630dc6e36f5b913e6563534019b0bd4e9c468
tony feng <thefunkyphresh@gmail.com>
2024-09-28 18:48:09 -0400

    specify indices for bevelize

--- src/utils/bevelize.js
+++ src/utils/bevelize.js
@@ -1,11 +1,19 @@
 const { calculateDistance, determineSigns, scaleVectors } = require("./vector");
 
-const bevelize = (points, bevelLength) => {
+const useSelectedIndices = (originalPoint, newValues, indices) => {
+	const newPoint = [...originalPoint];
+	indices.forEach((originalIndex, newIndex) => newPoint[originalIndex] = newValues[newIndex]);
+	return newPoint;
+};
+
+const bevelize = (points, bevelLength, indices = [0, 1]) => {
+	const pointsToBevel = points.map((point) => [point[indices[0]], point[indices[1]]])
 	const lastIndex = points.length - 1;
 	const result = [];
-	points.forEach((point, index) => {
-		const previousPoint = points[index === 0 ? lastIndex : index - 1];
-		const nextPoint = points[index === lastIndex ? 0 : index + 1];
+
+	pointsToBevel.forEach((point, index) => {
+		const previousPoint = pointsToBevel[index === 0 ? lastIndex : index - 1];
+		const nextPoint = pointsToBevel[index === lastIndex ? 0 : index + 1];
 		const [x0, y0] = previousPoint;
 		const [x1, y1] = point;
 		const [x2, y2] = nextPoint;
@@ -18,8 +26,10 @@ const bevelize = (points, bevelLength) => {
 		const [offsetX0, offsetY0] = scaleVectors([x0 - x1, y0 - y1, calculateDistance(previousPoint, point)], [,,offsetHyp]);
 		const [offsetX2, offsetY2] = scaleVectors([x2 - x1, y2 - y1, calculateDistance(nextPoint, point)], [,,offsetHyp]);
 
-		result.push([x1 + Math.abs(offsetX0) * previousOffsetSigns[0], y1 + Math.abs(offsetY0) * previousOffsetSigns[1]]);
-		result.push([x1 + Math.abs(offsetX2) * nextOffsetSigns[0], y1 + Math.abs(offsetY2) * nextOffsetSigns[1]]);
+		const previousOffset = [x1 + Math.abs(offsetX0) * previousOffsetSigns[0], y1 + Math.abs(offsetY0) * previousOffsetSigns[1]];
+		const nextOffset = [x1 + Math.abs(offsetX2) * nextOffsetSigns[0], y1 + Math.abs(offsetY2) * nextOffsetSigns[1]];
+		result.push(useSelectedIndices(points[index], previousOffset, indices));
+		result.push(useSelectedIndices(points[index], nextOffset, indices));
 	});
 
 	return result;
